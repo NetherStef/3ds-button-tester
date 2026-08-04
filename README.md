f6e190ec424e074f6f70cdf882402cb9beb8ccdb
stef <94950132+NetherStef@users.noreply.github.com>
2026-08-04 15:55:22 -0700

    Add files via upload
    V1.1.2 source code

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 Basic C program for the 3DS to test touch screen and button inputs.
 
-Pressing start will show you it works by returning you to the Homebrew menu, as so does the home button in a similar fashion.
-Pressing A + B will clear the console 
+Pressing start will show you it works by returning you to the Homebrew menu, as so does the home button in a similar fashion. Pressing A + B will clear the console
+
+For Users running Arch Linux (With devkitpro Installed) cd Into the folder with the source code inside
+
+$ cd /3ds-button-tester/
+
+$ make
+
+In the homebrew menu on the 3ds press "Y"
+Then in your Console type
+
+$ $DEVKITPRO/tools/bin/3dslink application.3dsx
+
+and it will show up on your 3ds
+
+Changelogs
+Added the On screen keyboard if X + Y are pressed.
+Changed kDown to kHeld on dual inputs to get more accurate input.
+Added version number 
+Changed the homebrew return buttons to LT and RT.
+Changed my name to my username.
--- source/main.c
+++ source/main.c
@@ -6,18 +6,25 @@
 
 int main(int argc, char* argv[])
 {
-	//Graphics initilize
-
+	//graphics I nitialize
 	gfxInitDefault();
-	consoleInit(GFX_TOP, NULL);
+	PrintConsole topConsole;
+	consoleInit(GFX_TOP, &topConsole);
+	consoleSelect(&topConsole);
 	touchPosition touch;
 
-    // Title \n makes a new line
-	printf("\033[31m                3ds Button Tester                 \033[0m");
-	printf("                By: Stefan Burner                 ");
-	printf("\033[36m    Press START to return to the Homebrew Menu.   \033[0m");
-	printf("\033[32m        Press A + B To clear the Console.         \033[0m\n");
-	//Mount SD Card (for future file manage tool)
+	printf("\x1b[1;0H\033[31m           3ds Button Tester v1.1.2               \033[0m\n");
+	printf("\x1b[2;0H                 By:  NetherStef                  \n");
+	printf("\x1b[3;0H\033[36m   Press LT + RT to return to the Homebrew Menu.  \033[0m\n");
+	printf("\x1b[4;0H\033[32m         Press A + B To clear the Console.        \033[0m\n");
+	printf("\x1b[5;0H         Press X + Y To open the keyboard.        \n");
+	printf("\x1b[6;0H--------------------------------------------------\n");
+
+// dont touch 24 it loves that.
+// also this just tells it the starting and ending like on the console
+	consoleSetWindow(&topConsole, 0, 7, 50, 24);
+
+// future sd card mount code block
 
 //	Result rc = qtmcInit();
 //	if (R_FAILED(rc)) {
@@ -38,12 +45,14 @@ int main(int argc, char* argv[])
 		gfxSwapBuffers();
 		hidScanInput();
 
-		// Your code goes here
 		u32 kHeld = hidKeysHeld();
 		u32 kDown = hidKeysDown();
-		if (kDown & KEY_START)
-			break; // break in order to return to hbmenu
+		// press home to return to hb
+
 		//Key Test
+
+		if (kDown & KEY_START)
+			printf("Start\n");
 		if (kDown & KEY_SELECT)
 			printf("Select\n");
 		if (kDown & KEY_A)
@@ -68,26 +77,42 @@ int main(int argc, char* argv[])
 			printf("RT\n");
 		if (kDown & KEY_TOUCH){
 			hidTouchRead(&touch);
-			printf("\x1b[5;0hTouch Sensed at X: %03d, Y: %03d\n", touch.px, touch.py);
+			printf("\x1b[5;0h%03d,%03d\n", touch.px, touch.py);
 		}
 
 
 
-		if (kHeld & KEY_CSTICK_DOWN)
+		if (kDown & KEY_CSTICK_DOWN)
 			printf("cDown\n");
-		if (kHeld & KEY_CSTICK_UP)
+		if (kDown & KEY_CSTICK_UP)
 			printf("cUp\n");
-		if (kHeld & KEY_CSTICK_LEFT)
+		if (kDown & KEY_CSTICK_LEFT)
 			printf("cLeft\n");
-		if (kHeld & KEY_CSTICK_RIGHT)
+		if (kDown & KEY_CSTICK_RIGHT)
 			printf("cRight\n");
 		if (kDown & KEY_ZL)
 			printf("ZL\n");
 		if (kDown & KEY_ZR)
 			printf("ZR\n");
-		if ((kDown & KEY_B) && (kDown & KEY_A))
+		if ((kHeld & KEY_B) && (kHeld & KEY_A))
 			consoleClear();
-
+		// Add on screen keyboard when X and Y are pressed, then output that text or cancel and make new // line.
+		if ((kHeld & KEY_X) && (kHeld & KEY_Y)){
+			static char kbBuffer[100];
+			SwkbdState swkbd;
+			swkbdInit(&swkbd, SWKBD_TYPE_NORMAL, 2, -1);
+			swkbdSetHintText (&swkbd, "3DS Button Tester");
+			swkbdSetValidation(&swkbd, SWKBD_NOTEMPTY_NOTBLANK, 0, 0);
+			SwkbdButton button = swkbdInputText(&swkbd, kbBuffer, sizeof(kbBuffer));
+			if (button == SWKBD_BUTTON_CONFIRM) {
+				printf("%s\n", kbBuffer);
+			} else {
+				printf("\n");
+			}
+		}
+		//Return to hb menu with LT and RT
+		if ((kHeld & KEY_R) && (kHeld & KEY_L))
+			break;
 
 
 
